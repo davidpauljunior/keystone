3948907a90f15998c7b4b8115f757dd732a8dc67
David Paul <davidpauljunior@gmail.com>
2017-01-20 15:59:59 +0000

    Changes db fallback path to my-site

--- keystone.js
+++ keystone.js
@@ -62,7 +62,7 @@ keystone.set('nav', {
 	users: 'users',
 });
 
-keystone.set('mongo', process.env.MONGO_URI || "mongodb://localhost:27017");
+keystone.set('mongo', process.env.MONGO_URI || "mongodb://localhost/my-site");
 
 if (keystone.get('env') == 'production'){
 	console.info('env = ' + keystone.get('env'));
